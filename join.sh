ac0ccab1dd4a7782be669dc68f553b96bf68e76d
Chris Fedun <chrisfedun031595@gmail.com>
2017-03-21 08:43:04 -0700

    Create join.sh

---
+++ join.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+######################################################################
+#
+#	Name:		 	join.sh
+#	Author:			Chris Fedun 21/03/2017
+#	Description:	Joins the Call Manager Dependencies package
+#	
+#	Copyright (C) 2017  Christopher Fedun
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+######################################################################
+
+cat callMan_deps.tar.bz2.partaa* >callMan_deps.tar.bz2
+
+####### END :) #######
